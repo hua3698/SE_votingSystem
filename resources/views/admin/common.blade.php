dccc785332ce358e4d254f4fec0ad2477955d239
koyuki <koyuki20000421@gmail.com>
2024-11-19 22:23:19 +0800

    koyuki

--- resources/views/admin/common.blade.php
+++ resources/views/admin/common.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>軟體工程第十一組 - </title>
+        <title>軟體工程第十一組 - koyuki</title>
         <link rel="icon" href="{{ asset('assets/ncu.avif') }}" type="image/x-icon">
         <link href="{{ asset('plugins/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
